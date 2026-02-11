418e7b0ee3becbaeb5ab3418d2935b90d554a5d9
v1bebtw <berdnikegor123@gmail.com>
2026-02-11 16:09:00 +0300

    Fix Front-end

--- js/script.js
+++ js/script.js
@@ -1,15 +1,6 @@
-console.log('Скрипт проекта EventSpace загружен!');
+const burger = document.getElementById("burger");
+const nav = document.getElementById("nav");
 
-function showWelcomeMessage() {
-    const now = new Date();
-    const hours = now.getHours();
-    let greeting;
-    
-    if (hours < 12) greeting = "Доброе утро!";
-    else if (hours < 18) greeting = "Добрый день!";
-    else greeting = "Добрый вечер!";
-    
-    console.log('${greeting} Добро пожаловать в EventSpace!');
-}
-
-showWelcomeMessage();
+burger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
